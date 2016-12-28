3b46151105864ba87ba902d92679e113dbdfd047
Keita Ito <keitaitok@gmail.com>
2016-12-27 18:47:47 -0800

    Initial Commit

---
+++ Panagram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Panagram
+//
+//  Created by Keita Ito on 12/27/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
